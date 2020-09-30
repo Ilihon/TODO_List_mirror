10ea8faf0264c447169e068727a5d41ffef8ba8c
imdeist <60685235+imdeist@users.noreply.github.com>
2020-10-01 02:25:42 +0700

    Update main.c and add new function add_chapter()

--- src/main.c
+++ src/main.c
@@ -1,1 +1,31 @@
+#include "todo.h"
 
+
+int main(int argc, char *argv[]){
+	char Text[250][250];
+	int i_buf[250];
+	while(1){
+	
+	int enter;
+	
+	setlocale(LC_ALL,"RUSSIAN");
+		printf("	Ваш личный блокнот\n");
+	printf("Нажмите одну из клавиш для дальнейшей работы: \n");
+	printf("1)Создание раздела\n2)Редактирование разделов\n3)Сохранить разделы\n4)Сагрузить разделы\n");
+	
+	
+	
+	int n;
+	scanf("%d",&enter);
+	switch(enter){
+		case 1:
+			
+			printf("Создайте номер раздела: ");
+			scanf("%d",&n); 
+			i_buf[n] = add_chapter(Text,n);
+			break;
+	}
+}
+	
+	return 0;
+}
