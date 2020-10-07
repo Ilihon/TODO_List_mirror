eb31093a4d14a2249e7e1ed1545f343bd4a711bc
imdeist <mihtym@gmail.com>
2020-10-07 10:23:07 +0700

    Update test.c

--- build/test/test.c
+++ build/test/test.c
@@ -2,6 +2,18 @@
 #include "todo.h"
 #include"ctest.h"
 
+CTEST(find_chapter,Chapter){
+	int check;
+	int i[250] = {0};
+	i[4] = 12;
+	for(int i = 0; i < 250;i++){
+		if(i_buf[i] != 0){
+			check = i_buf[i];
+		}
+	}
+	ASSERT_EQUAL(4,check);
+
+}
 CTEST(check_save_files,as){
 	char Text[250][250];
 	int check;
@@ -25,20 +37,4 @@ CTEST(Check_load_Texts,as){
 		if(i_buf[i] != 0) check = 0;
 	}
 	ASSERT_EQUAL(0,check);
-}
-CTEST(add_Ch_chek,Chapter){
-	char Text[250][250];
-	int i;
-	int N = 1;
-	i = add_chapter(Text,N);
-	ASSERT_EQUAL(4,i);
-
-}
-CTEST(check_edit,number_word){
-	char Text[250][250];
-	int i[250];
-	int N = 1;
-	i[N] = add_chapter(Text,N);
-	i[N] = edit(Text,N,i[N]);
-	ASSERT_EQUAL(8,i[N]);
 }
