9bd663afec52776199843bd9570a81f51bbc824d
Skitless <vovasotnikov11@gmail.com>
2020-10-07 09:43:22 +0700

    Fix headers on todo.h

--- build/src/todo.h
+++ build/src/todo.h
@@ -5,5 +5,5 @@
 
 int add_chapter(char Texts[250][250], int N);
 int edit(char Texts[250][250],int N, int i);
-void save(char Texts[250][250],int i[250]);
-void load(char Texts[250][250],int i[250]);
+int save(char Texts[250][250],int i[250]);
+int load(char Texts[250][250],int i[250]);
