9b499e01b3b9e696f61674f4234bd2136f6b48c1
Skitless <vovasotnikov11@gmail.com>
2020-10-07 10:32:45 +0700

    Update test.c

--- build/test/test.c
+++ build/test/test.c
@@ -18,7 +18,8 @@ CTEST(check_save_files,as){
 	char Text[250][250];
 	int check;
 	int i_buf[250] = {0};
-	i_buf[1] = add_chapter(Text,1);
+	Text[2] = "asd";
+	i_buf[2] = 3;
 	check = save(Text,i_buf);
 	ASSERT_EQUAL(0,check);
 }
