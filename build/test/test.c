374ab150c8012d8ec3bac6bb6802a9dc8987de34
tarbrin <tarbrin@mail.ru>
2020-10-07 10:03:56 +0700

    Add tests for load function

--- build/test/test.c
+++ build/test/test.c
@@ -4,9 +4,25 @@
 
 CTEST(check_save_files,as){
 	char Text[250][250];
-	
+	int check;
 	int i_buf[250] = {0};
 	i_buf[1] = add_chapter(Text,1);
-	int check = save(Text,i_buf);
+	check = save(Text,i_buf);
+	ASSERT_EQUAL(0,check);
+}
+CTEST(check_load_files,sa){
+	char Text[250][250];
+	int i_buf[250] = {0};
+	int check = load(Text,i_buf);
+	ASSERT_EQUAL(0,check);
+}
+CTEST(Check_load_Texts,as){
+	char Text[250][250];
+	int i_buf[250] = {0};
+	int check = 1;
+	load(Text,i_buf);
+	for(int i = 0; i < 250; i++){
+		if(i_buf[i] != 0) check = 0;
+	}
 	ASSERT_EQUAL(0,check);
 }
